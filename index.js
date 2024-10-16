b38d3ec5d6e55845c08f6248448a21e9e72e1222
Prince <princediman73573@gmail.com>
2024-10-16 12:32:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+class dbConnect{
+    constructor(connectionString){
+        this.connectionString=connectionString;
+    }
+   async getconn(){
+        try {
+            await mongoose.connect(this.connectionString);
+            console.log("db connected");
+        } catch (error) {
+            console.log("db conn error",error);
+            
+        }
+   } 
+}
+
+const value = process.env.MONGODB_URI;
+const newConn = new dbConnect(value);
+
+newConn.getconn();
+// module.exports = newConn;
